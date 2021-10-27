e1059e585c9c93d61e255fc9def650f778b76d4b
sgleass24 <89428432+sgleass24@users.noreply.github.com>
2021-10-26 20:52:45 -0600

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+// Create 4 variables. One called: "name", one called "pizzaCount", one called totalCost, and one called "isHungry".
+// The name variable should be set to your name.
+// The pizzaCount variable should be set to a number.
+// The isHungry variable should be set to true or false.
+// The totalCost variable should be set to pizzaCount variable * the cost of a pizza (you choose the cost)
+
+
+var firstName="Shelby";
+var pizzaCount="4";
+var totalCost=4*12;
+var isHungry="True";
+
+console.log("her name is" + " " + firstName + " " + "she ate" + " " + pizzaCount + " " + "pizzas, costing her $" + totalCost + " " +  firstName + " " + "was hungry:" + " " + isHungry);
